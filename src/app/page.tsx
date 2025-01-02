ea98625251698b3492940301e96735b8aa7cd4a2
atharv0811 <karnekaratharv12@gmail.com>
2025-01-02 15:46:04 +0530

    add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
+import About from "@/components/about";
+import AuthorSection from "@/components/author-section";
+import Blog from "@/components/blog";
+import Hero from "@/components/hero";
+import Projects from "@/components/projects";
+import Services from "@/components/services";
+import Testimonials from "@/components/testimonials";
 
 export default function Home() {
   return (
     <>
-      hello
+      <Hero />
+      <Services />
+      <About />
+      <Projects />
+      <Testimonials />
+      <AuthorSection />
+      <Blog />
     </>
   );
 }
